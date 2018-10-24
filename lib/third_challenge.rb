02f68f7f8630e8b2fb71aef60e57077beda302d7
Robin Kim <rkim1214@gmail.com>
2018-10-24 19:16:11 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -9,12 +9,8 @@ def third_challenge
    :capulet => {
       :patriarch => {:name => "Lord Capulet", :age => "50"},
       :matriarch => {:name => "Lady Capulet", :age => "51"},
-      :heroine => {:name => "Juliet", :age => "15"
-        :status => "alive"
-        },
+      :heroine => {:name => "Juliet", :age => "15", :status => "alive"},
       :heroine_friends => []
    }
   }
-
-  
 end
